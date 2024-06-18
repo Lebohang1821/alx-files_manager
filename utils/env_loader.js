88661018d1589913a910f25b801e6807a2ea97dc
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-06-18 12:52:48 +0200

    env

--- utils/env_loader.js
+++ utils/env_loader.js
@@ -1,12 +1,13 @@
 import { existsSync, readFileSync } from 'fs';
 
 /**
- * Loads the appropriate environment variables for an event.
+ * Loads appropriate environment variables for event.
  */
 const envLoader = () => {
   const env = process.env.npm_lifecycle_event || 'dev';
   const path = env.includes('test') || env.includes('cover') ? '.env.test' : '.env';
 
+   // It checks if specified environment file exists
   if (existsSync(path)) {
     const data = readFileSync(path, 'utf-8').trim().split('\n');
 
