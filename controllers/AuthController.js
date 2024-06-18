de20d79918401bbf6f6c62dbea518be958705a36
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-06-18 13:22:58 +0200

    AuthController

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-named-as-default */
 import { v4 as uuidv4 } from 'uuid';
 import redisClient from '../utils/redis';
 
@@ -6,11 +5,13 @@ export default class AuthController {
   static async getConnect(req, res) {
     const { user } = req;
     const token = uuidv4();
-// Storing authentication token in Redis with a 24-hour expiration
+
+    // Storing authentication token in Redis with a 24-hour expiration
     await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
     res.status(200).json({ token });
   }
-// Method to disconnect and invalidate authentication token
+
+  // Method to disconnect and invalidate authentication token
   static async getDisconnect(req, res) {
     const token = req.headers['x-token'];
 
