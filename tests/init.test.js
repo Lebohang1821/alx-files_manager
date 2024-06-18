312b59e5dfa7af634b947c0c16e71bb816a860c2
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-06-18 13:09:33 +0200

    Update init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -2,7 +2,8 @@ import supertest from 'supertest';
 import chai from 'chai';
 import api from '../server';
 
+// Setting up global variables for use in tests
 global.app = api;
 global.request = supertest(api);
-global.expect = chai.expect;
 global.assert = chai.assert;
+global.expect = chai.expect;
