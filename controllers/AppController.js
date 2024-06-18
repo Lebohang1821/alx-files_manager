94c017da7773ce12ce8f189dfc163b67bf7a2997
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-06-18 12:40:53 +0200

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,12 +4,13 @@ import dbClient from '../utils/db';
 
 export default class AppController {
   static getStatus(req, res) {
+     // Responds with the status of Redis and Database
     res.status(200).json({
       redis: redisClient.isAlive(),
       db: dbClient.isAlive(),
     });
   }
-
+   // Method to get statistics
   static getStats(req, res) {
     Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
       .then(([usersCount, filesCount]) => {
