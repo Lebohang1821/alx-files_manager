4964c3652893da4d779e468d19a85172f53ae784
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-06-18 12:11:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import startServer from './libs/boot';
 import injectRoutes from './routes';
 import injectMiddlewares from './libs/middlewares';
 
+// It initialize new Express application
 const server = express();
 
 injectMiddlewares(server);
